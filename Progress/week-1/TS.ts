2b6867fade871355ef6e5013c5be74374e816825
BemnetMussa <bemnetmussa@gmail.com>
2025-01-06 23:53:04 +0300

    ts basics ~ d6

--- Progress/week-1/TS.ts
+++ Progress/week-1/TS.ts
@@ -10,8 +10,51 @@
 // let name_: String = "Bemnet";
 // let dept: String = "SW";
 
-interface Person {
-  name: string;
-  age: number;
-}
 
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// function printId(id: string | number): void {
+//     console.log(`ID: ${id}`);
+// }
+
+// printId(101);    // Valid
+// printId("abc");   // Valid
+
+
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+// interface Contact {
+//     email: string;
+//     phone: string;
+// }
+
+// type Employee = Person & Contact;
+
+// const employee: Employee = {
+//     name: "Alice",
+//     age: 30,
+//     email: "alice@example.com",
+//     phone: "123-456-7890"
+// };
+
+
+
+// type Person = {
+//     name: string;
+//     age: number;
+// };
+
+// // Making all properties optional
+// type PartialPerson = {
+//     [K in keyof Person]?: Person[K];
+// };
+
+// const partialPerson: PartialPerson = {
+//     name: "Alice"
+// };
